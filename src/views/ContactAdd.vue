015972829ca2987004080d4d6f4c4c56d040cf1f
ngominhthien2004 <ngominhthien2222@gmail.com>
2025-06-29 18:17:22 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ mới</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="createContact"
+            @delete:contact="goBack"
+        />
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import contactService from "@/services/contact.service";
+
+export default {
+    components: { ContactForm },
+    data() {
+        return {
+            contact: {
+                name: "",
+                email: "",
+                address: "",
+                phone: "",
+                favorite: false,
+            },
+        };
+    },
+    methods: {
+        async createContact(contact) {
+            await contactService.create(contact);
+            this.$router.push({ name: "contactbook" });
+        },
+        goBack() {
+            this.$router.push({ name: "contactbook" });
+        },
+    },
+};
+</script>
